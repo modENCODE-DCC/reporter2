2c57ad97f43acd3edb181427fb66900bfc2b5138
zhengzha <zhengzha>
2009-05-14 20:51:03 +0000

    this version could work step-by-step, i.e, pause at soft file generation stage, pause at make tarball stage. it also could use existing soft file and existing tarball for next step of submission

--- chado2GEO.pl
+++ chado2GEO.pl
@@ -21,21 +21,28 @@ use Net::FTP;
 use Mail::Mailer;
 use Config::IniFiles;
 use Getopt::Long;
+use Digest::MD5;
 use ModENCODE::Parser::Chado;
 use GEO::Reporter;
 
 print "initializing...\n";
 
 #parse command-line parameters
-my ($unique_id, $output_dir, $config); 
+my ($unique_id, $output_dir, $config);
+#default config
+$config = $root_dir . 'chado2GEO.ini';
+my $use_existent_metafile = 0; 
 my $make_tarball = 0;
+my $use_existent_tarball = 0;
 my $send_to_geo = 0;
 my $option = GetOptions ("unique_id=s"     => \$unique_id,
 			 "out=s"           => \$output_dir,
 			 "config=s"        => \$config,
+			 "use_existent_metafile=s" => \$use_existent_metafile,
 			 "make_tarball=s"  => \$make_tarball,
+			 "use_existent_tarball=s"  => \$use_existent_tarball,   
 			 "send_to_geo=s"   => \$send_to_geo) or usage();
-usage() if (!$unique_id or !$output_dir or !$config);
+usage() if (!$unique_id or !$output_dir);
 usage() unless -w $output_dir;
 usage() unless -e $config;
 
@@ -48,66 +55,75 @@ my $report_dir = File::Spec->rel2abs($output_dir);
 #make sure $report_dir ends with '/'
 $report_dir .= '/' unless $report_dir =~ /\/$/;
 
-#what is the database for this dataset? 
-my $dbname = $ini{database}{dbname};
-my $dbhost = $ini{database}{host};
-my $dbusername = $ini{database}{username};
-my $dbpassword = $ini{database}{password};
-#search path for this dataset, this is fixed by modencode chado db
-my $schema = $ini{database}{pathprefix}. $unique_id . $ini{database}{pathsuffix} . ',' . $ini{database}{schema};
 #build the uniquename for this dataset
 my $unique_name = 'modencode_' . $unique_id;
 
-#start read chado
-print "connecting to database ...";
-my $reader = new ModENCODE::Parser::Chado({
+my ($seriesfile, $samplefile, $file_datafilenames, $metafile);
+$seriesfile = $report_dir . $unique_name . '_series.txt';
+$samplefile = $report_dir . $unique_name . '_sample.txt';
+$file_datafilenames = $report_dir . $unique_name . '_datafilenames.txt';
+$metafile = $unique_name . '.soft';
+
+if (($use_existent_metafile == 0) && ($use_existent_tarball == 0)) {
+    #what is the database for this dataset? 
+    my $dbname = $ini{database}{dbname};
+    my $dbhost = $ini{database}{host};
+    my $dbusername = $ini{database}{username};
+    my $dbpassword = $ini{database}{password};
+    #search path for this dataset, this is fixed by modencode chado db
+    my $schema = $ini{database}{pathprefix}. $unique_id . $ini{database}{pathsuffix} . ',' . $ini{database}{schema};
+
+    #start read chado
+    print "connecting to database ...";
+    my $reader = new ModENCODE::Parser::Chado({
 	'dbname' => $dbname,
 	'host' => $dbhost,
 	'username' => $dbusername,
 	'password' => $dbpassword,
-   });
-my $experiment_id = $reader->set_schema($schema);
-print "done.\n";
-print "loading experiment ...";
-$reader->load_experiment($experiment_id);
-my $experiment = $reader->get_experiment();
-print "done.\n";
+    });
+    my $experiment_id = $reader->set_schema($schema);
+    print "done.\n";
+    print "loading experiment ...";
+    $reader->load_experiment($experiment_id);
+    my $experiment = $reader->get_experiment();
+    print "done.\n";
 
-my $reporter = new GEO::Reporter({config => \%ini});
-my $seriesfile = $report_dir . $unique_name . '_series.txt';
-my $samplefile = $report_dir . $unique_name . '_sample.txt';
-
-my ($seriesFH, $sampleFH);
-open $seriesFH, ">", $seriesfile;
-open $sampleFH, ">", $samplefile;
-print "generating GEO series file ...";
-$reporter->chado2series($reader, $experiment, $seriesFH, $unique_name);
-print "done.\n";
-print "generating GEO sample file ...";
-my ($raw_datafiles, $normalized_datafiles) = $reporter->chado2sample($reader, $experiment, $seriesFH, $sampleFH, $report_dir);
-print "done.\n";
-close $sampleFH;
-close $seriesFH;
+    my $reporter = new GEO::Reporter({config => \%ini});
+    my ($seriesFH, $sampleFH);
+    open $seriesFH, ">", $seriesfile;
+    open $sampleFH, ">", $samplefile;
+    open my $file_datafilenamesFH, ">", $file_datafilenames;
+    print "generating GEO series file ...";
+    $reporter->chado2series($reader, $experiment, $seriesFH, $unique_name);
+    print "done.\n";
+    print "generating GEO sample file ...";
+    my ($raw_datafiles, $normalized_datafiles) = $reporter->chado2sample($reader, $experiment, $seriesFH, $sampleFH, $report_dir);
+    my @nr_raw_datafiles = nr(@$raw_datafiles);
+    my @nr_normalized_datafiles = nr(@$normalized_datafiles);
+    my @datafiles = (@nr_raw_datafiles, @nr_normalized_datafiles);
+    for my $datafile (@datafiles) {
+	print $file_datafilenamesFH $datafile, "\n";
+    }
+    chdir $report_dir;
+    my $file1 = basename($seriesfile);
+    my $file2 = basename($samplefile);
+    system("cat $file1 $file2 > $metafile") == 0 || die "can not catenate series and sample files to make soft file: $?";    
+    system("rm $file1 $file2") == 0 || die "can not remove the series and sample files: $?";
+    close $sampleFH;
+    close $seriesFH;
+    close $file_datafilenamesFH;
+    print "done\n";
+}
 
 my $tarfile = $unique_name . '.tar';
 my $tarballfile = $unique_name . '.tar.gz';
 my $tarball_made = 0;
-if ($make_tarball == 1) {
+if (($make_tarball == 1) && ($use_existent_tarball == 0)) {
     print "making tarball for GEO submission ...\n";
-    my @nr_raw_datafiles = nr(@$raw_datafiles);
-    my @nr_normalized_datafiles = nr(@$normalized_datafiles);
-    my @datafiles = (@nr_raw_datafiles, @nr_normalized_datafiles);
-    my $metafile = $unique_name . ".soft";
-    #make a tar ball at report_dir for series, sample files and all datafiles
+    #make a tar ball at report_dir for all datafiles
     chdir $report_dir;
-    my $dir = dirname($seriesfile);
-    my $file1 = basename($seriesfile);
-    my $file2 = basename($samplefile);
-    my @cat = ("cat $file1 $file2 > $metafile");
-    system(@cat) == 0 || die "can not cat two GEO meta files: $?";
-    my @tar = ('tar', 'cf', $tarfile, $metafile);
-    system(@tar) == 0 || die "can not make tar of two GEO meta files: $?";
-    system("rm $metafile") == 0 || die "can not remove catenated metafile: $?";
+    system("tar cf $tarfile $metafile") == 0 || die "can not tar the GEO soft file: $?";
+    system("rm $metafile") == 0 || die "can not remove GEO soft file: $?";
 
     print "   downloading tarball provided by pipeline ...";
     my $url = $ini{tarball}{url};
@@ -125,6 +141,15 @@ if ($make_tarball == 1) {
     print "done.\n";
     #peek into tarball to list all filenames
     @allfilenames = split(/\n/, `tar tzf "$allfile"`);
+    
+    my @datafiles;
+    open my $file_datafilenamesFH, "<", $file_datafilenames;
+    while (<$file_datafilenamesFH>) {
+	chomp;
+	push @datafiles, $_;
+    }
+    close $file_datafilenames;
+    system("rm $file_datafilenames") == 0 || die "can not remove file $file_datafilenames.";
 
     for my $datafile (@datafiles) {
 	#remove subdirectory prefix, this is the filename goes into geo tarball
@@ -133,13 +158,12 @@ if ($make_tarball == 1) {
 	$datafile =~ s/\//_/g;
 	#remove suffix of compression, such as .zip, .bz2
 	my $clean_file = unzipp($datafile);
-	
+	my $chars = 0 - length($clean_file);
 	my $filename_in_tarball;
 	for my $filename (@allfilenames) {
-          # Strip leading "extracted_"s
-          my $filename_without_extracted = $filename;
-          $filename_without_extracted =~ s/^(extracted_)*//;
-	    $filename_in_tarball = $filename and last if $filename_without_extracted eq $clean_file;
+	    # the filenames in pipeline provided tarball are of pattern extracted_maindirectory_subdirectory_datafilename,
+	    # the filenames in chado are of pattern subdirectory_datafilenames(_compression_suffix) 
+	    $filename_in_tarball = $filename and last if substr($filename, $chars) eq $clean_file;
 	}
 	my @untar = "tar xzf $allfile $filename_in_tarball";
 	system(@untar) == 0 || die "can not extract a datafile $filename_in_tarball from download tarball $allfile";
@@ -148,7 +172,7 @@ if ($make_tarball == 1) {
 	my @tar = ("tar -r --remove-files -f $tarfile $myfile");
 	system(@tar) == 0 || die "can not append a datafile $filename_in_tarball from download tarball $allfile to my tarball $tarfile and then remove it (leave no garbage).";
     }
-    system("rm $tarfile.gz 2>&1 > /dev/null"); # Remove the gzip if it already exists; ignore output
+    system("rm $tarballfile 2>&1 > /dev/null") if -e $tarballfile; # Remove the gzip if it already exists; ignore output
     my @tarball = ("gzip $tarfile");
     system(@tarball) == 0 || die "cannot gzip the tar file $tarfile";
     my @rm = ("rm $allfile");
@@ -157,8 +181,15 @@ if ($make_tarball == 1) {
     print "tarball made.\n";
 }
 
-if ($tarball_made && $send_to_geo) {
+if (($tarball_made || $use_existent_tarball) && $send_to_geo) {
     #use ftp to send file to geo ftp site
+    chdir $report_dir;
+    die "can not find tarball $tarballfile" unless -r $tarballfile;
+    my $md5 = Digest::MD5->new;
+    open my $tarballfh, "<", $tarballfile;
+    binmode $tarballfh;
+    $md5->addfile($tarballfh);
+    my $digest = $md5->hexdigest;
     print "beginning to send tarball to GEO ...\n";
     my $ftp_host = $ini{ftp}{host};
     my $ftp_username = $ini{ftp}{username};
@@ -166,11 +197,12 @@ if ($tarball_made && $send_to_geo) {
     my $ftp = Net::FTP->new($ftp_host);
     my $success = $ftp->login($ftp_username, $ftp_password);
     die $ftp->message unless $success;
-    my $success = $ftp->cwd($ini{ftp}{dir});
-    die $ftp->message unless $success;
-    my $success = $ftp->put($tarballfile);
-    die $ftp->message unless $success;
-
+    my $success1 = $ftp->cwd($ini{ftp}{dir});
+    die $ftp->message unless $success1;
+    $ftp->binary;
+    my $success2 = $ftp->put($tarballfile);
+    die $ftp->message unless $success2;
+    my $now_string = localtime;
     #send geo a email
     my $mailer = Mail::Mailer->new;
     my $submitter = $ini{submitter}{submitter};
@@ -183,6 +215,8 @@ if ($tarball_made && $send_to_geo) {
     print $mailer "userid: $submitter\n";
     print $mailer "file: $tarballfile\n";
     print $mailer "modencode DCC ID for this submission: $unique_id\n";
+    print $mailer "md5sum for this tarball is $digest\n";
+    print $mailer "sent successfully at $now_string\n";
     print $mailer "Best Regards, modencode DCC\n";
     $mailer->close;
     print "sent to GEO already!\n";
@@ -219,10 +253,16 @@ sub unzipp {
 }
 
 sub usage {
-    my $usage = qq[$0 -unique_id <unique_submission_id> -out <output_dir> -config <config_file> [-make_tarball <0|1>] [-send_to_geo <0|1>]];
+    my $usage = qq[$0 -unique_id <unique_submission_id> -out <output_dir> [-config <config_file>] [-use_existent_metafile <0|1>] [-make_tarball <0|1>] [-use_existent_tarball <0|1>] [-send_to_geo <0|1>]];
     print "Usage: $usage\n";
-    print "required parameters: unique_id, out, config\n";
+    print "example 1, generate soft file but no tarball, $0 -unique_id id -out dir \n";
+    print "example 2, generate tarball using existent soft file, $0 -unique_id id -out dir -use_existent_metafile 1 -make_tarball 1 \n";
+    print "example 3, use existent tarball to send to geo, $0 -unique_id id -out dir -use_existent_tarball 1 -send_to_geo 1 \n";
+    print "required parameters: unique_id, out\n";
+    print "optional parameter: config, for customized config file, the default one is chado2GEO.ini in this directory.\n";
+    print "optional parameter: use_existent_metafile, set to 1 if you already made GEO soft file and want to use it to make_tarball or send to geo. the file must exist in the output_dir and its name must be modencode_id.soft. another file named modencode_id_datafilenames.txt which contains all the data filenames must also exist in the output_dir. the script could then use these filenames to extract files in the tarball provided by pipeline.\n";
     print "optional yet helpful parameter: make_tarball, default is 0 for NOT archiving any raw/normalized data.\n";
+    print "optional yet helpful parameter: use_existent_tarball, default is 0. set to 1 if you have already made a tarball and want to use it to send to geo. the tarball must exist in the output_dir and its name must be modencode_id.tar.gz\n";
     print "optional yet important parameter: send_to_geo, default is 0 for NOT sending crappy results to geo. must set both make_tarball and send_to_geo to 1 for sending submission to geo happen.\n";
     exit 2;
 }
